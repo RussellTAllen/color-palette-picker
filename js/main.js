20c5c96472796cc69c7acae7bab59b47a1ffcb2b
RussellTAllen <russelltallen@gmail.com>
2021-01-20 02:09:16 -0600

    color palette picker

--- js/main.js
+++ js/main.js
@@ -2,20 +2,27 @@ const title = document.getElementById('title')
 const textColor = document.getElementById('text-color')
 const bgColor = document.getElementById('bg-color')
 const accentColor = document.getElementById('accent-color')
+const accentColor2 = document.getElementById('accent-color2')
+
 
 title.addEventListener('input', updatePage)
 textColor.addEventListener('input', updatePage)
 bgColor.addEventListener('input', updatePage)
 accentColor.addEventListener('input', updatePage)
+accentColor2.addEventListener('input', updatePage)
+
 
 function updatePage(){
     document.querySelector('h2').innerText = title.value
     document.querySelector('body').style.color = textColor.value
     document.querySelector('body').style.backgroundColor = bgColor.value
-    document.querySelector('section').style.borderColor = accentColor.value
+    document.querySelector('.accent').style.borderColor = accentColor.value
+    document.querySelector('.accent2').style.borderColor = accentColor2.value
     document.getElementById('text-value').innerText = textColor.value
     document.getElementById('bg-value').innerText = bgColor.value
     document.getElementById('accent-value').innerText = accentColor.value
+    document.getElementById('accent-value2').innerText = accentColor2.value
+
 }
 
 
